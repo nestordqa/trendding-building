c7dec16e82c4273829c848373a41d1ffc2f42b29
nestordqa <ndqa96@gmail.com>
2023-03-26 22:36:35 -0400

    students endpoint already working

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-PRISMA COMMANDS:
-prisma init (Crea el proyecto)
+lPRISMA COMMANDS:
 prisma generate (Crea el client)
 prisma studio (Abre una instancia en el navegador)
 prisma db push (Pushea modelos a DB)
---
+++ pages/api/students/index.ts
@@ -0,0 +1,107 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import { prisma } from '../../../lib/prisma';
+
+const students = async( req : NextApiRequest, res : NextApiResponse )=>{
+    const method = req.method;
+    const {
+        id, 
+        firstName, 
+        lastName, 
+        email, 
+        email_verified, 
+        gender, 
+        birthday, 
+        address, 
+        phone, 
+        city, 
+        province, 
+        country, 
+        photo, 
+        role, 
+        courses, 
+        active, 
+        createdAt, 
+        updatedAt
+    } = req.body;
+
+    switch (method) {
+        case 'GET':
+            try{
+                const students = await prisma.student.findMany({
+                    select: {
+                        id: true,
+                        firstName: true,
+                        lastName: true,
+                        email: true,
+                        gender: true,
+                        phone: true,
+                        city: true,
+                        province: true,
+                        country: true,
+                        role: true,
+                        courses:{
+                            select:{
+                                tittle: true
+                            }
+                        },
+                        active: true,
+                        createdAt: true
+                    }
+                });
+                if(students.length <= 0){
+                    res.status(404).json({message: 'There are not studetns yet!'});
+                };
+                if(students.length > 0){
+                    res.status(200).json(students);
+                };
+            }catch(error){
+                res.status(400).json({error: error})
+            }            
+        break;
+        
+        case 'POST':
+            try{
+                const finding = await prisma.student.findUnique({
+                    where:{
+                        email: email
+                    }
+                });
+                if(finding){
+                     return res.status(401).json({message: 'User already exist.'})
+                }
+                if(!firstName || !lastName || !email || !email_verified || !gender || !birthday || !address || !phone || !city || !province || !country || !photo){
+                    return res.status(401).json({error: 'Missing data!'})
+                }
+                if(!finding){
+                    const newStudent = await prisma.student.create({
+                        data:{
+                            firstName,
+                            lastName,
+                            email,
+                            email_verified,
+                            gender,
+                            birthday,
+                            address,
+                            phone,
+                            city,
+                            province,
+                            country,
+                            photo,
+                            role,
+                            active: true,
+                            createdAt: new Date()
+                        }
+                    });
+                    return res.status(201).json(newStudent);
+                }
+            }catch(error){
+                res.status(401).json({error: error})
+            }        
+        break;
+        default:
+            res.status(503).json({error: 'Bad request, invalid method'})
+            break;
+    }
+};
+
+export default students;
